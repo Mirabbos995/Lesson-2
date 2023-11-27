6e8dba7909469a5f5ab35becdf0d3bbb2ff63dc6
Mirabbos995 <jahonaliyevmirabbos621@gmail.com>
2023-11-27 11:47:28 +0300

    firstproject

---
+++ main2.py
@@ -0,0 +1,14 @@
+import math
+
+print(5**4) # 625 bo'ladi 1-misol
+
+print(22%4) # 2 qoldiq qoladi 2-misol
+
+print(125**2) # kvadratning yuzi 3-misol
+
+print(125+125+125+125) # yoki 125*4. kvning perimetri 4-misol
+print(3.14*12/2*12/2) # doiraning yuzi 113.4
+
+
+print(6**2 + 7**2) #5-misol
+print(math.sqrt(85/1)) #javob 9.21
